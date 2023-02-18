831aa13b15a615a6ded590d1329715df53b1535f
huangwenqiang <wqao1023@qq.com>
2023-02-18 11:43:24 +0800

    delete all

--- src/views/purchase/certification.vue
+++
@@ -1,5 +0,0 @@
-<template>
-	<div>certification</div>
-</template>
-
-<script lang="ts" setup></script>
--- src/views/system/contact.vue
+++
@@ -1,7 +0,0 @@
-<template>
-	<contact-list :read="true" />
-</template>
-
-<script lang="ts" setup>
-import contactList from "@/components/contact-list.vue";
-</script>
